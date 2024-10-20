607166b6a1d4562670d945ed2f8487cf79481b4a
Hanna Lakus <annalakus@ukr.net>
2024-10-20 14:52:37 +0100

    added model

---
+++ src/db/models.js
@@ -0,0 +1,18 @@
+import mongoose, { model } from 'mongoose';
+
+const ProductsSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: {
+      type: String,
+      required: true,
+      enum: ['books', 'electronics', 'clothing', 'other'],
+      default: 'other',
+    },
+    description: { type: String, required: false },
+  },
+  { timestamps: true },
+);
+
+export const ProductCollection = model('products', ProductsSchema);
